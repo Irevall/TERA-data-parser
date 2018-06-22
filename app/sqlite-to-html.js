9eda894cf2775fdf3c832a7cd4b28be262d8fcb4
Irevall <kubajmarek@gmail.com>
2018-06-22 13:01:59 +0200

    Simplify informations in table

--- app/sqlite-to-html.js
+++ app/sqlite-to-html.js
@@ -6,6 +6,33 @@ function logAndExit(error) {
     process.exit();
 }
 
+function dungeonsScore(data) {
+    let html = '';
+    if (/party/.exec(data) !== null) {
+        let x = data.split(' ');
+        let number = x[2].replace(/,/g, '');
+        html += (number / (10**6)).toFixed(1);
+        html += 'M ';
+        html += x[3];
+        html += ')';
+    } else if (/Divine/.exec(data) !== null){
+        let x = data.match(/[0-9]+[%]/g);
+        x.forEach((element, index) => {
+            html += '<img src="buffs/priest/buff' + (index + 1) + '.png" alt="Buff" />';
+            html += ' ' + element;
+        });
+    } else if (/Thrall/.exec(data) !== null) {
+        let x = data.match(/[0-9]+[%]/g);
+        x.forEach((element, index) => {
+            html += '<img src="buffs/mystic/buff' + (index + 1) + '.png" alt="Buff" />';
+            html += ' ' + element;
+        });
+    } else {
+        html += data;
+    }
+    return html;
+}
+
 function buildHtml(data) {
     let head = '';
     let body = '';
@@ -13,7 +40,7 @@ function buildHtml(data) {
     head += '<link rel="stylesheet" type="text/css" href="style.css"/>';
     head += '<script src="main.js"></script>';
 
-    body += '<table><tr><th>Name</th><th>Rank</th><th>Class</th><th>Contribution</th><th>Last online</th><th>Note</th><th>RKE</th><th>RRHM</th><th>TRNM</th><th>AANM</th><th>RKNM</th><th>Discord</th><th>Civil Unrest</th></tr>';
+    body += '<table><tr><th>Name</th><th>Rank</th><th>Class</th><th>Contribution</th><th>Last online</th><th>Note</th><th>RKE</th><th>RRHM</th><th>TRNM</th><th>AANM</th><th>RKNM</th><th>Misc</th></tr>';
     data.forEach((element) => {
         body += '<tr><td>';
         body += element.name;
@@ -22,25 +49,24 @@ function buildHtml(data) {
         body += '</td><td>';
         body += element.class;
         body += '</td><td>';
-        body += element.contrCurrent + '(' + element.contrTotal + ')';
+        body += element.contrCurrent + ' (' + element.contrTotal + ')';
         body += '</td><td>';
-        body += element.lastOnline;
-        body += '</td><td>';
+        body += element.lastOnline.split(',')[0];
+        body += '</td><td class="hidden"><div class="empty">...</div><div class="content">';
         body += element.note;
-        body += '</td><td>';
-        body += element.RKE;
-        body += '</td><td>';
-        body += element.RRHM;
-        body += '</td><td>';
-        body += element.TRNM;
-        body += '</td><td>';
-        body += element.AANM;
-        body += '</td><td>';
-        body += element.RKNM;
-        body += '</td><td>';
-        body += element.discord ? '<input type="checkbox" value="has-discord" checked="checked">' : '<input type="checkbox" value="has-discord">';
-        body += '</td><td>';
-        body += element.civil ? '<input type="checkbox" value="plays-civil" checked="checked">' : '<input type="checkbox" value="plays-civil">';
+        body += '</div></td><td class="dungeons hidden"><div class="empty">...</div><div class="content">';
+        body += dungeonsScore(element.RKE);
+        body += '</div></td><td class="dungeons hidden"><div class="empty">...</div><div class="content">';
+        body += dungeonsScore(element.RRHM);
+        body += '</div></td><td class="dungeons hidden"><div class="empty">...</div><div class="content">';
+        body += dungeonsScore(element.TRNM);
+        body += '</div></td><td class="dungeons hidden"><div class="empty">...</div><div class="content">';
+        body += dungeonsScore(element.AANM);
+        body += '</div></td><td class="dungeons hidden"><div class="empty">...</div><div class="content">';
+        body += dungeonsScore(element.RKNM);
+        body += '</div></td><td>';
+        body += element.discord ? '<img src="icons/discord-blue.svg" alt="Has discord" />' : '<img src="icons/discord-grey.svg" alt="No discord" />';
+        body += element.civil ? '<img src="icons/sword-blue.svg" alt="Plays civil unrest" />' : '<img src="icons/sword-grey.svg" alt="Doesn\'t play civil unrest" />';
         body += '</td></tr>'
     });
     body += '</table>';
--- html/main.js
+++ html/main.js
@@ -13,6 +13,23 @@ function checkChanges() {
     });
 }
 
+function hideElements() {
+    document.querySelectorAll('th').forEach((element, index) => {
+        if (index >= 5 && index <= 11) {
+            element.addEventListener('click', () => {
+                document.querySelectorAll('tr').forEach((element2, index2) => {
+                    if (index2 === 0) {
+                        return false;
+                    } else {
+                        element2.querySelectorAll('td')[index].classList.remove('hidden');
+                        element2.querySelectorAll('td')[index].classList.add('visible');
+                    }
+                });
+            })
+        }
+    });
+}
 document.addEventListener('DOMContentLoaded', () => {
     checkChanges();
+    hideElements();
 });
